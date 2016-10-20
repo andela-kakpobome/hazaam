0dd981ce18c928d26b3bd98e6064e3d7eba23b00
andela-bmakinwa <makinwa37@gmail.com>
2016-10-20 17:50:35 +0100

    Include NG2 route module

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [
+      BrowserModule,
+      RouterModule.forRoot([
+        { path: 'fpaas', component: AppComponent }
+      ])
+    ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
 })
