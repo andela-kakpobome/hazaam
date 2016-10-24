3e46d9ab4ed3c9fd172e15be459e1cc32a3ba83a
andela-bmakinwa <makinwa37@gmail.com>
2016-10-24 19:17:07 +0100

    Create fingerprint component files

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,18 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule }   from '@angular/router';
 import { FpaasComponent } from './fpaas/fpaas.component'
+import { FingerPrintComponent } from './fpaas/fingerprint.component'
 
 @NgModule({
     imports: [
       BrowserModule,
       RouterModule.forRoot([
         { path: '', component: AppComponent },
-        { path: 'fpaas', component: FpaasComponent }
+        { path: 'fpaas', component: FpaasComponent },
+        { path: 'fpaas/fingerprint', component: FingerPrintComponent }
       ])
     ],
     declarations: [
       AppComponent,
-      FpaasComponent
+      FpaasComponent,
+      FingerPrintComponent
     ],
     bootstrap: [ AppComponent ]
 })
---
+++ client/app/fpaas/fingerprint/fingerprint.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fingerprint',
+  templateUrl: './app/fpaas/fingerpint/fingerprint.component.html',
+  styleUrls: ['./app/fpaas/fingerpint/fingerprint.component.css']
+})
+
+export class FingerPrintComponent {}
