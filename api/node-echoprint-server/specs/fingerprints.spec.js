3bebc8cd4d79bd5732770912dd2ddf46f8d3e875
kesiena.akp <kesiena.akpobome@andela.com>
2016-10-25 00:33:30 +0100

    Set up Mocha testing environment for echoprint server

---
+++ api/node-echoprint-server/specs/fingerprints.spec.js
@@ -0,0 +1,11 @@
+const should = require('should');
+const request = require('supertest');
+
+describe('Fingerprint Resource',function () {
+
+  it('should pass this test',function () {
+
+    //(5).should.be.exactly(5).and.be.a.Number();
+    "hi".should.be.an.instanceof.String;
+  });
+});
