4fb513d86310c49184bdbdd08d6f4ae0be99c9dd
kesiena.akp <kesiena.akpobome@andela.com>
2016-10-25 16:30:05 +0100

    Remove commented line in test spec

--- api/node-echoprint-server/specs/fingerprints.spec.js
+++ api/node-echoprint-server/specs/fingerprints.spec.js
@@ -5,7 +5,6 @@ describe('Fingerprint Resource',function () {
 
   it('should pass this test',function () {
 
-    //(5).should.be.exactly(5).and.be.a.Number();
     "hi".should.be.an.instanceof.String;
   });
 });
