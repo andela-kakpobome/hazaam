d4e003e24bf3fce1eacd97360243e97e4000546d
andela-bmakinwa <makinwa37@gmail.com>
2016-10-27 18:29:31 +0100

    Create fpaas module

--- client/app/fpaas/fingerprint/fingerprint.component.ts
+++ client/app/fpaas/fingerprint/fingerprint.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'fingerprint',
-  templateUrl: './app/fpaas/fingerpint/fingerprint.component.html',
-  styleUrls: ['./app/fpaas/fingerpint/fingerprint.component.css']
+  templateUrl: './app/fpaas/fingerprint/fingerprint.component.html',
+  styleUrls: ['./app/fpaas/fingerprint/fingerprint.component.css']
 })
 
 export class FingerPrintComponent {}
