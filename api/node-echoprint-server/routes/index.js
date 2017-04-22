dfefaddda06170bfdfbabb0c4adcfede05373c85
Makinwa Bukola <bukola.makinwa@andela.com>
2017-04-22 16:03:09 +0100

    Update gitignore file

--- api/node-echoprint-server/routes/index.js
+++ api/node-echoprint-server/routes/index.js
@@ -6,22 +6,22 @@
 */
 
 const express = require('express');
+
 const router = express.Router();
 
 const fingerprintRoutes = require('./fingerprints');
 
-//create new express instance to mount resource routes
+// create new express instance to mount resource routes
 const mount = new express();
 
-module.exports = function () {
-
-  //define default route
-  mount.get('/',(req, res) => {
-    res.status(200).send({message:'Welcome to the Hazaam API'});
+module.exports = () => {
+  // define default route
+  mount.get('/', (req, res) => {
+    res.status(200).send({ message: 'Welcome to the Hazaam API' });
   });
 
-  //mount routes for the fingerprint resource
-  mount.use('/fingerprints',fingerprintRoutes(router));
+  // mount routes for the fingerprint resource
+  mount.use('/fingerprints', fingerprintRoutes(router));
 
   return mount;
 };
